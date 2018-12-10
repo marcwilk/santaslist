3eaec1e16d46d2eeaad6e173e564946bf138eed2
Marc <marc.wilk@colorado.edu>
2018-12-10 14:30:14 -0700

    routes up to date

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,13 @@ var express = require('express')
 var router = express.Router()
 var knex = require('../knex')
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   knex('users')
   .then((data) => {
     res.send(data)
+  })
+  .catch(err => {
+    next(err)
   })
 })
 
@@ -16,6 +18,47 @@ router.get('/:id', function(req, res, next) {
   .first()
   .then((data) => {
     res.send(data)
+  })
+  .catch(err => {
+    next(err)
+  })
+})
+
+router.post('/', function(req, res, next) {
+  knex('users')
+  .insert(req.body, '*')
+  .then((data) => {
+    res.send(data)
+  })
+  .catch(err => {
+    next(err)
+  })
+})
+
+router.patch('/:id', function(req, res, next) {
+  knex('users')
+  .where('id', req.params.id)
+  .update(req.body)
+  .returning('*')
+  .then((data) => {
+    res.send(data)
+  })
+  .catch(err => {
+    next(err)
+  })
+})
+
+router.delete('/:id', function(req, res, next) {
+  knex('users')
+  .where('id', req.params.id)
+  .first()
+  .returning('*')
+  .del()
+  .then((data) => {
+    res.send(data)
+  })
+  .catch(err => {
+    next(err)
   })
 })
 
