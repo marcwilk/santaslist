bfd6d469da58178be54b60ee24cb311ad57bc014
Marc <marc.wilk@colorado.edu>
2018-12-10 15:33:49 -0700

    added html/js

---
+++ public/javascripts/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  let ul = document.querySelector('ul')
+  axios.get('http://localhost:3000/users')
+  .then((results) => {
+    for (let i = 0; i < results.data.length; i++) {
+      let li = document.createElement('li')
+      li.innerText = `${results.data[i].name},
+      ${results.data[i].isNice}`
+      ul.appendChild(li)
+    }
+  })
+})
